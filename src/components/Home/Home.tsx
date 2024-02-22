2c18f5ac9d1340710ee196a2f2fe9b43e11f2ed0
Sachin <74880556+sachin691@users.noreply.github.com>
2024-02-22 19:51:24 +0530

    Home page (#1)
    * add home and navbar
    
    * finish setup
    
    * finish

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -4,13 +4,24 @@ import { useDispatch } from "react-redux";
 // Local Files
 import { updateTab } from "../../store/curTabSlice";
 import { scrollTop } from "../../utils/controllers";
+import Hero from "./subComponents/Hero";
+import Meal from "./subComponents/Meal";
+import PerfectMeal from "./subComponents/PerfectMeal";
+import OurStory from "./subComponents/OurStory";
 
 const Home = () => {
   const dispatch = useDispatch();
   dispatch(updateTab("Home"));
   scrollTop();
 
-  return <div>Home</div>;
+  return (
+    <>
+      <Hero />
+      <Meal />
+      <PerfectMeal />
+      <OurStory />
+    </>
+  );
 };
 
 export default Home;
---
+++ src/components/Home/subComponents/Meal.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Meal = () => {
+  return (
+    <div className="flex items-center justify-center h-auto text-center bg-black">
+      <p className="text-center text-2xl sm:text-3xl md:text-4xl px-[2rem] py-[2rem] md:py-[10rem] md:px-[10rem] lg:px-[18rem] text-white font-bold">
+        Unleash your kitchen bravery with our seafood, matching your fearless spirit in life. Infused with exhilarating
+        flavors and prepared in under 30 minutes, our chefs have paved the way, empowering you to craft the flawless
+        meal.
+      </p>
+    </div>
+  );
+};
+
+export default Meal;
