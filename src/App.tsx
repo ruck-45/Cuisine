34fabe7ac328e9e0783b6afd9881744a044984a8
Sachin <sachinkaurav65@gmail.com>
2024-02-29 00:06:03 +0530

    back to basics

--- src/App.tsx
+++ src/App.tsx
@@ -72,7 +72,8 @@ function App() {
       <div style={{ backgroundImage: background }} className={className}>
         {curTab === "Auth" || curTab === "Password Reset" || curTab === "Robinhood" ? null : <NavBar />}
         <Routes>
-          <Route path="/" element={<Robinhood />} />
+          {/* <Route path="/" element={<Robinhood />} /> */}
+          <Route path="/" element={<Navigate to="/Home" />} />
           <Route path="/Home" element={<Home />} />
           <Route path="/About" element={<About />} />
           <Route path="/Contact" element={<Contact />} />
