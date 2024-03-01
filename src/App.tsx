81f969e7c5b2a3d060ad19ab7eb16c259b5a9e42
AnishMishra1 <Anishmishra@gmail.com>
2024-03-01 12:22:16 +0530

    change to cusine

--- src/App.tsx
+++ src/App.tsx
@@ -72,10 +72,10 @@ function App() {
       <div style={{ backgroundImage: background }} className={className}>
         {curTab === "Auth" || curTab === "Password Reset" || curTab === "Robinhood" ? null : <NavBar />}
         <Routes>
-          <Route path="/Home" element={<Robinhood />} />
-          <Route path="/" element={<Robinhood />} />
-          {/* <Route path="/" element={<Navigate to="/Home" />} /> */}
-          {/*<Route path="/Home" element={<Home />} /> */}
+          {/* <Route path="/Home" element={<Robinhood />} />
+          <Route path="/" element={<Robinhood />} /> */}
+          <Route path="/" element={<Navigate to="/Home" />} />
+          <Route path="/Home" element={<Home />} />
           <Route path="/About" element={<About />} />
           <Route path="/Contact" element={<Contact />} />
           <Route path="/Recipes" element={<Recipes />} />
@@ -83,7 +83,7 @@ function App() {
           <Route path="/OurStory" element={<Story />} />
           <Route path="/Sustain" element={<Sustain />} />
           <Route path="/landing" element={<Landing />} />
-          {/* <Route path="*" element={<Navigate to="/Home" />} /> */}
+          <Route path="*" element={<Navigate to="/Home" />} />
         </Routes>
       </div>
       {curTab === "Auth" || curTab === "Password Reset" || curTab === "Robinhood" ? null : <Footer />}
